c2787f6de6c283d0343308cdf0536b4b4812f408
kenji <kenjix12x22@gmail.com>
2021-12-17 00:26:00 +0900

    createアクション作成、bcryptインストール

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else 
+      render 'new'
+    end
+  end
+
   def edit
   end
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_cofirmation)
+    end
 end
