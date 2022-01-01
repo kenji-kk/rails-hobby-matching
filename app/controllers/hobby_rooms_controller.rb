7e73a26912e078d9ef8a6ecc3953b67ffe0ca1a1
kenji <kenjix12x22@gmail.com>
2022-01-01 23:57:55 +0900

    hobby_room作成機能実装

--- app/controllers/hobby_rooms_controller.rb
+++ app/controllers/hobby_rooms_controller.rb
@@ -6,6 +6,19 @@ class HobbyRoomsController < ApplicationController
   end
 
   def create
+    @hobby_room = HobbyRoom.new(hobby_room_params)
+    if @hobby_room.save
+      redirect_to hobby_rooms_index_path
+    else
+      render 'index'
+    end
   end
-  
+
+
+
+  private
+
+    def hobby_room_params
+      params.require(:hobby_room).permit(:genre, :title, :description, :user_id)
+    end
 end
