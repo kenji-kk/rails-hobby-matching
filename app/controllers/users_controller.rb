69c8c3b7d995b31257f9d6d8825d74c90798d958
kenji <kenjix12x22@gmail.com>
2022-01-08 23:47:34 +0900

    session_controllerにログインを要求する機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, except: [:new, :login]
+  before_action :logged_in_user, except: [:new]
 
   def index
     @users = User.all
