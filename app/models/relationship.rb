f01e939b29f1648797110e7974fc6c69443fea25
kenji <kenjix12x22@gmail.com>
2022-01-10 22:09:31 +0900

    follow関係のアソシエーション設定

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,6 @@
 class Relationship < ApplicationRecord
+  belogns_to :follower, class_name: "User"
+  belogns_to :followed, class_name: "User"
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
 end
