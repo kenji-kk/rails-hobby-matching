b27586493a93e966d743634b773a3b6abc5db2d2
kenji <kenjix12x22@gmail.com>
2022-01-14 21:38:26 +0900

    searchメソッドをモデルに追記

--- app/models/hobby_room.rb
+++ app/models/hobby_room.rb
@@ -6,5 +6,8 @@ class HobbyRoom < ApplicationRecord
   validates :genre, presence: true, length: { maximum: 50}
   validates :title, presence: true, length: { maximum: 100}
   validates :description, presence: true, length: { maximum: 300 }
+
+  def self.search(search)
+    where(['genre LIKE ?', "%#{search}%"])
   
 end
