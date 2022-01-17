b68f5286135724d32ebe778cf4d4165e7a2dd8c5
kenji <kenjix12x22@gmail.com>
2022-01-17 23:28:49 +0900

    コントローラ側で一度いいねしてたらできないように実装

--- app/controllers/like_rooms_controller.rb
+++ app/controllers/like_rooms_controller.rb
@@ -1,13 +1,13 @@
 class LikeRoomsController < ApplicationController
   def create
-    unless current_user.like_rooms.include?(params[:room_id])
+    unless current_user.like_rooms.include?(LikeRoom.find_by(hobby_room_id: params[:room_id]))
       like = current_user.like_rooms.new(hobby_room_id: params[:room_id])
       if like.save 
         flash[:success] = "投稿に「いいね！」しました。"
         redirect_back(fallback_location: hobby_rooms_path)
       end
     else
-      flash[:alert] = 'すでに「いいね！」しています。'
+      flash[:danger] = 'すでに「いいね！」しています。'
       redirect_back(fallback_location: hobby_rooms_path)
     end
   end
