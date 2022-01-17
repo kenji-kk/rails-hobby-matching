3efd16e4f40f206434fee1aa0a6b728dac711f60
kenji <kenjix12x22@gmail.com>
2022-01-17 22:10:49 +0900

    モデルのアソシエート追記

--- app/models/hobby_room.rb
+++ app/models/hobby_room.rb
@@ -3,6 +3,7 @@ class HobbyRoom < ApplicationRecord
   has_many :hobby_room_users
   has_many :exist_users, through: :hobby_room_users, source: :user
   has_many :group_chats
+  has_many :like_room
   validates :genre, presence: true, length: { maximum: 50}
   validates :title, presence: true, length: { maximum: 100}
   validates :description, presence: true, length: { maximum: 300 }
