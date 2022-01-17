b8829fcff10d4199a0323143059b6d0bb53ddc62
kenji <kenjix12x22@gmail.com>
2022-01-17 22:53:33 +0900

    いいね機能実装（何回でもできる状態）

--- app/controllers/like_rooms_controller.rb
+++ app/controllers/like_rooms_controller.rb
@@ -1,2 +1,12 @@
 class LikeRoomsController < ApplicationController
+  def create
+    like = current_user.like_room.new(hobby_room_id: params[:room_id])
+    if like.save 
+      flash[:success] = "投稿に「いいね！」しました。"
+      redirect_back(fallback_location: hobby_rooms_path)
+    else
+      flash[:alert] = '「いいね！」に失敗しました。'
+      redirect_back(fallback_location: hobby_rooms_path)
+    end
+  end
 end
