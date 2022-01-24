e3fbfa8887fc83880299ea156056604d434dc26b
kenji <kenjix12x22@gmail.com>
2022-01-25 01:01:34 +0900

    entryモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_155605) do
+ActiveRecord::Schema.define(version: 2022_01_24_160038) do
+
+  create_table "entries", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_entries_on_room_id"
+    t.index ["user_id"], name: "index_entries_on_user_id"
+  end
 
   create_table "group_chats", force: :cascade do |t|
     t.integer "hobby_room_id", null: false
@@ -92,6 +101,8 @@ ActiveRecord::Schema.define(version: 2022_01_24_155605) do
     t.string "remember_digest"
   end
 
+  add_foreign_key "entries", "rooms"
+  add_foreign_key "entries", "users"
   add_foreign_key "group_chats", "hobby_rooms"
   add_foreign_key "hobby_room_users", "hobby_rooms"
   add_foreign_key "hobby_room_users", "users"
